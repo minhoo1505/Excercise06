ca67bb753ea41651d0ac1043a795342ad862599f
thaibinh <thienthan1455@gmail.com>
2019-10-15 17:28:28 +0700

    Exercise06

---
+++ ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Account
+    {
+        private int MONEY;
+        public Account(int amount)
+        {
+            MONEY = amount;
+        }
+        public void Deposit(int amount)
+        {
+            MONEY += amount;
+        }
+        public void Withdraw(int amount)
+        {
+            MONEY -= amount;
+        }
+        public  int Balance()
+        {
+            return MONEY;
+        }
+    }
+}
